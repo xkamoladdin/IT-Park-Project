20a47ab21cc6ef49c524e4851bed19277e1f95b4
kamoladdin <kamoladdin0709@gmail.com>
2025-04-15 15:03:17 +0500

    start-nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Courses from "./Pages/Courses";
 import NotFound from "./Pages/NotFound";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />        
         <Route path="/about" element={<About />} />        
+        <Route path="/courses" element={<Courses />} />        
         {/* 404 sahifa uchun */}
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,13 +3,36 @@ import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <nav>
+    <nav className="container py-[20px] flex justify-between">
+
       <NavLink exact to="/" activeClassName="active">
-        Home
+        CREATE <span>X</span>
       </NavLink>
-      <NavLink to="/about" activeClassName="active">
-        About
-      </NavLink>
+      <span>
+        <NavLink to="/about" activeClassName="active">
+          About Us
+        </NavLink>
+        <NavLink to="/courses" activeClassName="active">
+          Courses
+        </NavLink>
+        <NavLink to="/events" activeClassName="active">
+          Events
+        </NavLink>
+        <NavLink to="/blog" activeClassName="active">
+          Blog
+        </NavLink>
+        <NavLink to="/contacts" activeClassName="active">
+          Contacts
+        </NavLink>
+      </span>
+      <span>
+        <NavLink to="/about" activeClassName="active">
+          Get consultation
+        </NavLink>
+        <NavLink to="/login" activeClassName="active">
+          Log in / Register
+        </NavLink>
+      </span>
     </nav>
   );
 }
