1728fdcffdfcbfa89654700c241a7c85af8a9e9d
JamshidbekAtajonov <jamshidbekatajonov757@gmail.com>
2025-04-15 16:15:59 +0500

    event-start

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react'
 export default class Events extends Component {
   render() {
     return (
-      <div>Events</div>
+      <div className='container flex'>
+        <div className='text-center'>
+        <h2 className='text-[#FF3F3A] text-align mt-[120px]'>Online lecture</h2>
+        <h1 className='font-black text-[30px] mb-[120px]'>Formation of the organizational structure of the company in the face of uncertainty</h1>
+        </div>
+        <section>
+          <div className='flex'>
+            <div>
+              <h1>We will talk about:</h1>
+
+            </div>
+          </div>
+        </section>
+      </div>
     )
   }
 }
