3c538ae738e967b4fec356cb59b6fd0fff768517
azizbek1701 <azizbekkamilov1701@gmail.com>
2025-04-15 16:12:28 +0500

    nav_course

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -10,9 +10,9 @@ export default class Courses extends Component {
             <h1 className='font-lato text-[46px] font-bold'>Our online courses</h1>
           </div>
           <div className='flex items-center justify-between font-lato my-[60px]'>
-            <ul className='flex gap-[32px] text-[16px] text-[#9A9CA5]'>
+            <ul className='flex gap-[32px] text-[16px] text-[#9A9CA5] items-center'>
               <a href="#">
-                <li className='text-[#FF3F3A] border-2 border-red-500 rounded-lg'>All</li>
+                <li className='text-[#FF3F3A] border-2 border-red-500 rounded-lg py-[10px] px-[20px]'>All</li>
               </a><a href="#">
                 <li>Marketing</li>
               </a><a href="#">
@@ -26,6 +26,26 @@ export default class Courses extends Component {
               </a>
             </ul>
             <input className='border-2 border-gray-500 rounded-lg py-[11px] px-[16px] w-[315px]' type="search" name="" id="" placeholder='Search course...'/>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                
+              </div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
