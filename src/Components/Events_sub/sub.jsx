bdc2b2424aca10efefb3643224b81b305acccf65
JamshidbekAtajonov <jamshidbekatajonov757@gmail.com>
2025-04-15 18:03:22 +0500

    sdlfijsjdfhgsudf

---
+++ src/Components/Events_sub/sub.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Bruh from "../Events_sub/illustration.png"
+export default class sub extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+            <div>
+                <img src={Bruh} alt="" />
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
