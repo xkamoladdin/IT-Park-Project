d4bd012021163fb72a9e37643b5c08221eca7224
sobirjon <sobirjonotajonov11@gmail.com>
2025-04-15 15:04:34 +0500

    about start

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react'
 export default class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <div>
+
+
+        <div>
+        <div>
+          <h2>About Us</h2>
+          <h1>Createx Online School</h1>
+          <p>Createx Online School is a leader in online studying. We have lots of courses and programs from the main market experts.</p>
+          <p>We provide relevant approaches to online learning, internships and employment in the largest companies in the country. Our educational programs help you get a new specialty from scratch. During your studies, we will help you find a job. Check the courses and online events that we organise.</p>
+        </div>
+        <div></div>
+        </div>
+
+      </div>
     )
   }
 }
