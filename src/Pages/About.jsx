d158ed98c22fdde5a560234faabaa115fa953e4b
sobirjon <sobirjonotajonov11@gmail.com>
2025-04-15 15:27:46 +0500

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -12,6 +12,8 @@ export default class About extends Component {
           <h1>Createx Online School</h1>
           <p>Createx Online School is a leader in online studying. We have lots of courses and programs from the main market experts.</p>
           <p>We provide relevant approaches to online learning, internships and employment in the largest companies in the country. Our educational programs help you get a new specialty from scratch. During your studies, we will help you find a job. Check the courses and online events that we organise.</p>
+          <button>Explore events</button>
+          <button>Browse courses</button>
         </div>
         <div></div>
         </div>
