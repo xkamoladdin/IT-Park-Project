aea38482a39678791bd064a35fe02237f7b2c2e2
azizbek1701 <azizbekkamilov1701@gmail.com>
2025-04-16 16:20:32 +0500

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,6 +6,60 @@ function Footer() {
     <footer>
       {/* <p></p> */}
 
+      <div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>Createx Online School is a leader in online studying. We have lots of courses and programs from the main market experts. We provide relevant approaches to online learning, internships and employment in the largest companies in the country. </p>
+            <div>
+
+            </div>
+          </div>
+          <div>
+            <h6>SITE MAP</h6>
+            <ul>
+              <a href="#">
+                <li>Courses</li>
+              </a>
+              <a href="#">
+                <li>Courses</li>
+              </a>
+              <a href="#">
+                <li>Courses</li>
+              </a>
+              <a href="#">
+                <li>Courses</li>
+              </a>
+              <a href="#">
+                <li>Courses</li>
+              </a>
+            </ul>
+          </div>
+          <div>
+            <h6>COURSES</h6>
+            <ul>
+              <a href="#">
+                <li>Marketing</li>
+              </a>
+              <a href="#">
+                <li>Marketing</li>
+              </a>
+              <a href="#">
+                <li>Marketing</li>
+              </a>
+              <a href="#">
+                <li>Marketing</li>
+              </a>
+              <a href="#">
+                <li>Marketing</li>
+              </a>
+            </ul>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
     </footer>
   );
 }
